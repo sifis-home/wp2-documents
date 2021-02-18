d6a7ce5decb770ce515dd31e8036668c94db7192
Luni-4 <luni-4@hotmail.it>
2021-02-18 14:27:02 +0100

    Add chapter 4 to final deliverable

--- wp2-deliverable-2.1/deliverable.sh
+++ wp2-deliverable-2.1/deliverable.sh
@@ -4,7 +4,8 @@
 set -e
 
 MAIN_FILE=wp2-deliverable-2-1.md
-MD_FILES="software-assessment.md security-privacy.md conclusion.md annex.md"
+MD_FILES="software-assessment.md security-privacy.md \
+          privacy-and-security-assessment-techniques.md conclusion.md annex.md"
 CITATIONS_FILENAME=citations
 OUTPUT_FILENAME=wp2-deliverable-2.1
 OUTPUT_DIR=../output/$OUTPUT_FILENAME
